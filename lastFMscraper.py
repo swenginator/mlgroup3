bb4f611f3fdb0bd2d97327c07ce736d8d7bc4082
swenginator <72451636+swenginator@users.noreply.github.com>
2022-11-23 13:24:43 +0000

    Reformat

--- lastFMscraper.py
+++ lastFMscraper.py
@@ -1,24 +1,24 @@
 import requests
 import json
-import time
-import pandas as pd
 
-def getFriends(n, user, listOfFriends):
+
+def get_friends(n, user, friends_list):
     request_url = url1.format("friends", user, key, limit, extended, page)
     response = requests.get(request_url).json()
     for item in response["friends"]["user"]:
-        if item not in listOfFriends:
-            listOfFriends.append(item["name"])
+        if item not in friends_list:
+            friends_list.append(item["name"])
     if n == 0:
-        return listOfFriends
+        return friends_list
     else:
-        for friend in listOfFriends:
-            getFriends(n-1,friend, listOfFriends)
-    return listOfFriends
+        for friend in friends_list:
+            get_friends(n - 1, friend, friends_list)
+    return friends_list
 
-def getSongs(user):
-    method='toptracks'
-    
+
+def get_songs(user):
+    method = 'toptracks'
+
     request_url = url.format(method, username, key, limit, extended)
     response = requests.get(request_url).json()
     pages = response[method]["@attr"]["totalPages"]
@@ -28,13 +28,13 @@ def getSongs(user):
 
         allSongs = []
 
-        f = open("UserSongList\\"+user+".txt","w")
+        f = open("UserSongList\\" + user + ".txt", "w")
 
         for item in response[method]["track"]:
             if item not in allSongs:
-                f.write(item["name"]+", "+item["mbid"]+"\n")
+                f.write(item["name"] + ", " + item["mbid"] + "\n")
         f.close()
-        
+
 
 pause_duration = 0.2
 key = "b5dd048101c7fb6285afcb0398ec3e58"
@@ -44,13 +44,13 @@ username = "rj"
 url1 = 'https://ws.audioscrobbler.com/2.0/?method=user.get{}&user={}&api_key={}&limit={}&extended={}&page={}&format=json'
 url = 'https://ws.audioscrobbler.com/2.0/?method=user.get{}&user={}&api_key={}&limit={}&extended={}&format=json'
 
-
-
-limit = 200; extended = 0; page = 1
+limit = 200
+extended = 0
+page = 1
 
 sampleUsers = [username]
-sampleUsers = getFriends(0, username, sampleUsers)
+sampleUsers = get_friends(0, username, sampleUsers)
 print(sampleUsers)
 for i in sampleUsers:
-    getSongs(i)
-    print(i+" complete!")
+    get_songs(i)
+    print(i + " complete!")
