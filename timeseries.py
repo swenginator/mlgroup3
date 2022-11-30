d40e5a403414cf8ba63db9dc5081287ec3064c0b
swenginator <72451636+swenginator@users.noreply.github.com>
2022-11-30 18:10:22 +0000

    Remove timeseries model

--- timeseries.py
+++ timeseries.py
@@ -2,15 +2,12 @@ import os
 import json
 import pandas
 import re
-import numpy as np
-
-from sklearn.linear_model import Ridge
-from sklearn.model_selection import train_test_split
-
+import numpy
 
 SAVED_PATH = "saved"
 
 
+# Returns dataframe with each row as playedsong and columns with name of each tag
 def load_into_pandas():
     for filename in os.listdir(SAVED_PATH):
         print(filename)
@@ -28,7 +25,7 @@ def load_into_pandas():
                     filtered_tags = []
                     count = 0
                     for tag in top_tags:
-                        if count > 5:
+                        if count > 0:  # Only get top tag
                             break
                         name = tag['name'].replace('-', '').lower()
                         weight = int(tag['weight'])
@@ -39,7 +36,6 @@ def load_into_pandas():
 
                     # Add tags to list of played tracks
                     played_tracks.append({
-                        'timestamp': played_track['timestamp'],
                         'title': played_track['title'],
                         'artist': played_track['artist']['name'],
                         'album': played_track['album']['name'],
@@ -52,8 +48,8 @@ def load_into_pandas():
             # each row is a recorded track
             # each column is a tag
 
-    #print("Found tags:")
-    #print(found_tags)
+    # print("Found tags:")
+    # print(found_tags)
 
     # We need a dictionary where keys are tags and values are lists
     ligma = dict()
@@ -64,35 +60,27 @@ def load_into_pandas():
             taglist = ligma[tag]
             taglist.append(1 if tag in track['tags'] else 0)
 
-
     df = pandas.DataFrame(ligma)
     print(df)
+    return df
 
-    #print("Played tracks:")
-    #[print(track) for track in played_tracks]
 
-def test_preds(q, lag, input):
-    stride = 1
-    X1 = input[0:input.size - q - lag: stride]
-    for i in range(1,lag):
-        X = input[i:input.size - q - (lag - i): stride]
-        X1 = np.column_stack((X1,X))
-    input1 = input[lag + q: stride]
-    train,test = train_test_split(np.arange(0,input.size),test_size=0.2)
-    
-    model = Ridge(fit_intercept = False).fit(X1[train],input1[train])
-    
-    ret = ()
-    y_pred = model.predict(X1[test])
-    ret.append(y_pred)
-    y = model.predict(input[test])
-    ret.append(y)
-    return ret
-    
+# Take in dataframe and return trained model
+def train_model(df):
+    print("Training model...")
+    # TODO
+
+
+# Take in trained model and last few played tracks for specific user
+# and predict most likely tracks
+def make_prediction(model, played_tracks):
+    print("Predicting...")
+    # TODO predict and just print out top ten for now
+
 
 def main():
-    load_into_pandas()
-    test_preds(1,0,holder)
+    df = load_into_pandas()
+    breakpoint()
 
 
 if __name__ == "__main__":
