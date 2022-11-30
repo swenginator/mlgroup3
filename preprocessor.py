a79e7a13e0af72b9cc9d41bd4b7ba8ed8c33f399
swenginator <72451636+swenginator@users.noreply.github.com>
2022-11-30 19:20:15 +0000

    Pre process data to csv

---
+++ preprocessor.py
@@ -0,0 +1,66 @@
+import os
+import json
+import re
+
+SAVED_PATH = "saved"
+OUTPUT_NAME = "playedtracks.csv"
+
+# Process user listened tracks and saves data into csv
+# Have to use dict instead of set because it's sorted
+found_labels = dict()  # Store all unique occurrences of each tag, artist name, album name
+played_tracks = []
+
+for filename in os.listdir(SAVED_PATH):
+    # check if current path is a file
+    filepath = os.path.join(SAVED_PATH, filename)
+    if os.path.isfile(filepath):
+        with open(filepath) as file:
+            print(f'User: {filename}')
+            for line in file:
+                played_track = json.loads(line)
+                top_tags = played_track['top_tags']
+                artist_name = played_track['artist']['name']
+                album_name = played_track['album']['name']
+
+                filtered_labels = []
+                if artist_name is not None:
+                    filtered_labels.append(artist_name)
+                    found_labels[artist_name] = None
+                if album_name is not None:
+                    filtered_labels.append(album_name)
+                    found_labels[album_name] = None
+
+                # Grab tag names and remove years
+                count = 0
+                for label in top_tags:
+                    if count > 5:  # Only get top 5 tags for each track
+                        break
+                    name = label['name'].replace('-', '').lower()
+                    weight = int(label['weight'])
+                    if len(re.findall(r'\d+', name)) <= 0 and weight > 50:
+                        filtered_labels.append(name)
+                        found_labels[name] = None
+                        count += 1
+
+                # Add tags to list of played tracks
+                played_tracks.append(filtered_labels)
+
+with open(OUTPUT_NAME, "w") as file:
+    # Write column headers
+    column_headers = list(found_labels)
+    for i in range(len(column_headers) - 1):
+        file.write(column_headers[i] + ',')
+
+    file.write(column_headers[len(column_headers) - 1] + '\n')
+
+    for played_track in played_tracks:
+        row_values = list()
+        for label in found_labels.keys():
+            value = '1' if label in played_track else '0'
+            row_values.append(value)
+
+        for i in range(len(row_values) - 1):
+            file.write(row_values[i] + ',')
+
+        # Add last item in row without comma but with newline instead
+        file.write(row_values[len(row_values) - 1] + '\n')
--- trainer.py
+++ trainer.py
@@ -1,69 +1,7 @@
-import os
-import json
 import pandas
-import re
 import numpy
 
-SAVED_PATH = "saved"
-
-
-# Returns dataframe with each row as playedsong and columns with name of each tag
-def load_into_pandas():
-    # Have to use dict instead of set cause it's sorted
-    found_labels = dict()  # Store all unique occurrences of each tag, artist name, album name
-    played_tracks = []
-
-    for filename in os.listdir(SAVED_PATH):
-        # check if current path is a file
-        filepath = os.path.join(SAVED_PATH, filename)
-        if os.path.isfile(filepath):
-            with open(filepath) as file:
-                print(f'User: {filename}')
-                for line in file:
-                    played_track = json.loads(line)
-                    top_tags = played_track['top_tags']
-                    artist_name = played_track['artist']['name']
-                    album_name = played_track['album']['name']
-
-                    filtered_labels = [artist_name, album_name]
-                    found_labels[artist_name] = None
-                    found_labels[album_name] = None
-
-                    # Grab tag names and remove years
-                    count = 0
-                    for label in top_tags:
-                        if count > 5:  # Only get top 5 tags for each track
-                            break
-                        name = label['name'].replace('-', '').lower()
-                        weight = int(label['weight'])
-                        if len(re.findall(r'\d+', name)) <= 0 and weight > 50:
-                            filtered_labels.append(name)
-                            found_labels[name] = None
-                            count += 1
-
-                    # Add tags to list of played tracks
-                    played_tracks.append(filtered_labels)
-
-            # df = pandas.DataFrame(track_dict)
-            # print(df)
-            # each row is a recorded track
-            # each column is a tag
-
-    # print("Found tags:")
-    # print(found_tags)
-
-    # We need a dictionary where keys are labels and values are lists
-    df_dict = dict()
-    for track in played_tracks:
-        for label in found_labels.keys():
-            if label not in df_dict:
-                df_dict[label] = list()
-            labellist = df_dict[label]
-            labellist.append(1 if label in track else 0)
-
-    df = pandas.DataFrame(df_dict)
-    print(df)
-    return df
+CSV_PATH = "playedtracks.csv"
 
 
 # Take in dataframe and return trained model
@@ -80,9 +18,8 @@ def make_prediction(model, played_tracks):
 
 
 def main():
-    df = load_into_pandas()
+    df = pandas.read_csv(CSV_PATH)
     print(f"Data loaded into dataframe of shape {df.shape}")
-    breakpoint()
     model = train_model(df)
     print(f"Model trained, intercept: {model.intercept_}, coefs: {model.coef_}")
 
