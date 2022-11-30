a509dbfee8a903e9ecaf120435ea7e868ec85beb
swenginator <72451636+swenginator@users.noreply.github.com>
2022-11-30 18:33:32 +0000

    Add artist and album name as columns

--- trainer.py
+++ trainer.py
@@ -10,7 +10,7 @@ SAVED_PATH = "saved"
 # Returns dataframe with each row as playedsong and columns with name of each tag
 def load_into_pandas():
     # Have to use dict instead of set cause it's sorted
-    found_tags = dict()  # Store all unique occurrences of each tag
+    found_labels = dict()  # Store all unique occurrences of each tag, artist name, album name
     played_tracks = []
 
     for filename in os.listdir(SAVED_PATH):
@@ -22,25 +22,27 @@ def load_into_pandas():
                 for line in file:
                     played_track = json.loads(line)
                     top_tags = played_track['top_tags']
+                    artist_name = played_track['artist']['name']
+                    album_name = played_track['album']['name']
+
+                    filtered_labels = [artist_name, album_name]
+                    found_labels[artist_name] = None
+                    found_labels[album_name] = None
+
                     # Grab tag names and remove years
-                    filtered_tags = []
                     count = 0
-                    for tag in top_tags:
+                    for label in top_tags:
                         if count > 5:  # Only get top 5 tags for each track
                             break
-                        name = tag['name'].replace('-', '').lower()
-                        weight = int(tag['weight'])
+                        name = label['name'].replace('-', '').lower()
+                        weight = int(label['weight'])
                         if len(re.findall(r'\d+', name)) <= 0 and weight > 50:
-                            filtered_tags.append(name)
-                            found_tags[name] = None
+                            filtered_labels.append(name)
+                            found_labels[name] = None
                             count += 1
 
                     # Add tags to list of played tracks
-                    played_tracks.append({
-                        'title': played_track['title'],
-                        'artist': played_track['artist']['name'],
-                        'album': played_track['album']['name'],
-                        'tags': filtered_tags})
+                    played_tracks.append(filtered_labels)
 
             # df = pandas.DataFrame(track_dict)
             # print(df)
@@ -50,14 +52,14 @@ def load_into_pandas():
     # print("Found tags:")
     # print(found_tags)
 
-    # We need a dictionary where keys are tags and values are lists
+    # We need a dictionary where keys are labels and values are lists
     df_dict = dict()
     for track in played_tracks:
-        for tag in found_tags.keys():
-            if tag not in df_dict:
-                df_dict[tag] = list()
-            taglist = df_dict[tag]
-            taglist.append(1 if tag in track['tags'] else 0)
+        for label in found_labels.keys():
+            if label not in df_dict:
+                df_dict[label] = list()
+            labellist = df_dict[label]
+            labellist.append(1 if label in track else 0)
 
     df = pandas.DataFrame(df_dict)
     print(df)
@@ -79,7 +81,10 @@ def make_prediction(model, played_tracks):
 
 def main():
     df = load_into_pandas()
+    print(f"Data loaded into dataframe of shape {df.shape}")
     breakpoint()
+    model = train_model(df)
+    print(f"Model trained, intercept: {model.intercept_}, coefs: {model.coef_}")
 
 
 if __name__ == "__main__":
