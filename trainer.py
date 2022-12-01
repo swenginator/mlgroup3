d46dc2f10699ac2c8ea8a4f3dc13e38b0ff5bc62
swenginator <72451636+swenginator@users.noreply.github.com>
2022-12-01 15:33:04 +0000

    Use nearest neighbours for training

--- trainer.py
+++ trainer.py
@@ -2,7 +2,6 @@ import pandas
 import numpy
 import pickle
 
-from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.neighbors import NearestNeighbors
 
 CSV_PATH = "playedtracks.csv"
@@ -12,13 +11,15 @@ MODEL_NAME = "model.sav"
 # Take in dataframe and return trained model
 def train_model(df):
     print("Training model...")
-    data_similarity = cosine_similarity(df.T)
-    print(data_similarity)
-    data_similarity_df = pandas.DataFrame(data_similarity, columns=df.columns, index=df.columns)
-    neigh = NearestNeighbors(n_neighbors=len(df.columns)).fit(data_similarity_df)
-    model = pandas.DataFrame(neigh.kneighbors(data_similarity_df, return_distance=False))
+    # Similarity between each played track and every other played track, shape df rows x df rows
+    neigh = NearestNeighbors(n_neighbors=len(df.index), algorithm='brute', metric='cosine').fit(X=df)
+    # TODO make prediction on the test data not the training data
+    predictions = neigh.kneighbors(X=df.iloc[:1], n_neighbors=15, return_distance=False)
+    for indices in predictions:
+        for i in indices:
+            print(df.columns[i])
 
-    return pandas.DataFrame(data_similarity_df.columns[model], index=data_similarity_df.index)
+    return neigh
 
 
 def main():
